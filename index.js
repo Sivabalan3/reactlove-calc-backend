a4b07369c85fd16d2c29824e023c00a0fcef29d6
Sivabalan <sivabalanm205@gmail.com>
2024-05-21 08:58:59 +0530

    db error

--- index.js
+++ index.js
@@ -6,10 +6,11 @@ require("dotenv").config();
 const app = express();
 app.use(cors());
 app.use(express.json());
-const connection = mongoose.connection;
-
 const url = process.env.ATLAS_URL;
-
+mongoose.connect(url, {
+  useNewUrlParser: true,
+  useUnifiedTopology: true,
+});
 const nameSchema = new mongoose.Schema({
   name1: String,
   name2: String,
@@ -26,7 +27,7 @@ app.post("/names", async (req, res) => {
 
 app.listen(5000, () => console.log("Server started on port 5000"));
 
-mongoose.connect(url);
-connection.once("open", () => {
-  console.log("mongoose db connected sucessfully");
+const connection = mongoose.connection;
+connection.once('open', () => {
+  console.log('Mongoose DB connected successfully');
 });
